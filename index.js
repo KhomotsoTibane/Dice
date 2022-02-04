2290b7805e482c773a1261089ceb05a9bb94ddcd
KTibane13 <96890432+KTibane13@users.noreply.github.com>
2022-02-05 01:52:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+
+var start = false;
+$(".btn-refresh").click(function(){
+
+  if(!start){
+    rollDice();
+    start = true;
+  }
+start = false;
+});
+
+
+function rollDice(){
+
+var randomNumber1 = Math.floor(Math.random() *6) + 1;
+
+var randomDiceImage1 = "dice"+randomNumber1+".png";
+
+var randomImageSource1 = "images/"+randomDiceImage1;
+
+ var image1 = document.querySelectorAll('img')[0];
+
+ image1.setAttribute("src" , randomImageSource1);
+
+
+var randomNumber2 = Math.floor(Math.random() *6) + 1;
+
+var randomDiceImage2 = "dice"+randomNumber2+".png";
+
+var randomImageSource2 = "images/"+randomDiceImage2;
+
+var image2 = document.querySelectorAll('img')[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+
+  document.querySelector("h1").innerHTML = ("🚩🚩🚩Player 1 wins!!")
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = ("🚩🚩🚩Player 2 wins!!")
+}
+else{
+    document.querySelector("h1").innerHTML = ("It's a draw.")
+}
+}
